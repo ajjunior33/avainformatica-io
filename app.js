a6336b6e48e85bd98ab9ec47e2f0891b9f7d7455
ajjunior33 NWT <ajjunior33@gmail.com>
2019-10-11 08:37:20 -0300

    Atualização do app

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.get("/", (req, res) => {
-	res.send("<h1> Olá, mundo!</h1>");
+app.get("/", function(req, res){
+	res.send("<h1> Olá, mundo!");
 });
 
-app.listen(3000, () => console.log("server rodando..."))
+
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
+	console.log("Umbler listening on port %s", port);
+});
