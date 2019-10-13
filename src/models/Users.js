b78225e2fdd159a4810cabc318a2514a7bd9f879
Andrezao <ava.info.contato@gmail.com>
2019-10-13 16:34:30 -0300

    Limpando Repositorio

--- src/models/Users.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const UserSchema = new mongoose.Schema({
-    email: String,
-});
-
-
-// Mandando o Mango usar a estrutura do UserSchema
-module.exports = mongoose.model('User', UserSchema);
