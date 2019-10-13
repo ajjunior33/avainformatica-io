4fd5b44dabd80ecb96e412c4c1791d69a8293165
Andrezao <ava.info.contato@gmail.com>
2019-10-13 18:43:54 -0300

    Alter

--- src/server.js
+++ src/server.js
@@ -4,6 +4,9 @@ var app = express();
 app.get("/", (req, res) => {
     return res.send("<h1>Amor</h1>");
 }); 
+app.get("/login", (req, res) => {
+    return res.json({"login" : "logar-se"});
+}); 
                                 
 var port = process.env.andre || 3000;
 app.listen(port, function () {
