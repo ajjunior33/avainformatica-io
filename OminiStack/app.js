13d18d510eb91a668bf3dd483f59d74cd4aab927
ajjunior33 NWT <ajjunior33@gmail.com>
2019-10-24 09:00:48 -0300

    Alterações na estrutura de arquivo.

--- OminiStack/app.js
+++ OminiStack/app.js
@@ -3,7 +3,8 @@ var app = express();
 
 //... your code here ...
 app.get("/", function(req, res){
-	res.send("<h1> Olá, mundo!");
+    res.send("<h1> Olá, mundo!");
+    console.log('Umbler listening on port %s', port);
 });
 
 var port = process.env.andre || 3000;
