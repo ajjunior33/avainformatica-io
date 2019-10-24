fe380c3830742be6986c32b111694857899b50ab
ajjunior33 NWT <ajjunior33@gmail.com>
2019-10-24 12:19:26 -0300

    Alterações

---
+++ sqlnode/src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: 'mysql',
+  host: 'mysql380.umbler.com',
+  username: 'ajjunior33',
+  password: 'andreregedit',
+  port: '41890',
+  database: 'suporteava',
+  define: {
+    timestamps: true,
+
+  }
+
+}
