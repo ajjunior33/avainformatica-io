b0e4c801ffcc458809559a587ab9f5fec1a194b6
Andre Junior <=>
2020-02-14 12:23:19 -0300

    alters

--- src/app.js
+++ src/app.js
@@ -4,9 +4,8 @@ const app = express();
 
 app.get("/", (req, res) => {
     console.log("hello, world");
-    res.json({mesager: "hello, world"});
+    res.json({ mesager: "hello, world" });
 });
 
-app.listen(3333, () => {
-    console.log('rodando na 3333 ')
-})
+
+app.listen(process.env.andre || 3333, () => console.log("Escutando!"));
