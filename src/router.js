fb667a0f9bf30fdba1a22682dfee65a5ad8b142f
André Junior <ajjunior33@gmail.com>
2020-05-03 05:38:03 -0300

    Formaterr

--- src/router.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express");
-
-const route = Router();
-
-const mercadoPagoController = require("./app/controllers/mercadoPago");
-route.get("/mercadoPago", mercadoPagoController.taxaCartao);
-
-module.exports = route;
